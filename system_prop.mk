645efa264a10e2631ee5463be425dc49f9d26632
UtsavisGreat <utsavbalar1231@gmail.com>
2020-01-11 08:22:09 +0000

    temp
    Signed-off-by: UtsavisGreat <utsavbalar1231@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -1,20 +1,18 @@
+# Bluetooth
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.btstack.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aptxadaptive-aac-ldac \
+    persist.vendor.btstack.enable.splita2dp=true \
+    vendor.bluetooth.soc=cherokee
+
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
-    vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.qualcomm.saltproject2,com.miui.cit,com.android.camera \
     persist.camera.sat.fallback.dist=40 \
     persist.camera.sat.fallback.dist.d=10 \
-    persist.camera.sat.fallback.luxindex=360 \
-    persist.camera.sat.fallback.lux.d=50
-
-# Display density
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sf.lcd_density=440
-
-# Display features
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.displayfeature.histogram.enable=true \
-    ro.eyecare.brightness.threshold=11 \
-    ro.eyecare.brightness.level=5
+    persist.camera.sat.fallback.luxindex=310 \
+    persist.camera.sat.fallback.lux.d=50 \
+    persist.vendor.camera.enableNCSService=TRUE \
+    persist.vendor.camera.enableTOFInterface=TRUE \
+    vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,org.lineageos.snap
 
 # CNE and DPM
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -22,74 +20,87 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.dpm.feature=1 \
     persist.vendor.dpm.nsrm.bkg.evt=3955
 
-# Data modules
+# Data Modules
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.data.df.dev_name=rmnet_usb0 \
-    persist.vendor.data.profile_update=true \
     persist.vendor.data.mode=concurrent \
     ro.vendor.use_data_netmgrd=true
 
-# Display post-processing
+# Display
 PRODUCT_PROPERTY_OVERRIDES += \
+    ro.eyecare.brightness.threshold=11 \
+    ro.eyecare.brightness.level=5 \
+    ro.displayfeature.histogram.enable \
+    ro.hist.brightness.threshold=7 \
+    ro.sf.lcd_density=440 \
     ro.vendor.display.ad=1 \
     ro.vendor.display.ad.hdr_calib_data=/vendor/etc/hdr_config.cfg \
     ro.vendor.display.ad.sdr_calib_data=/vendor/etc/sdr_config.cfg \
-    ro.vendor.display.sensortype=2
+    ro.vendor.display.sensortype=2 \
+    ro.xiaomi.bl.poll=true \
+    persist.displayfeature.dc_backlight.threshold=610 \
+    persist.displayfeature.dc_backlight.enable=false \
+    persist.fod.modified.dc_status=false \
+    sys.displayfeature.hbm.enable=true
 
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
-    debug.sf.enable_gl_backpressure=1 \
-    debug.sf.enable_hwc_vds=1 \
-    debug.sf.early_phase_offset_ns=500000 \
-    debug.sf.early_app_phase_offset_ns=500000 \
-    debug.sf.early_gl_phase_offset_ns=3000000 \
-    debug.sf.early_gl_app_phase_offset_ns=15000000 \
-    ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
-    ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
-    ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
+    debug.sf.disable_backpressure=1 \
+    debug.sf.enable_hwc_vds=1
 
-# IMS
+# GPS
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.dbg.volte_avail_ovr=1 \
-    persist.dbg.vt_avail_ovr=1 \
-    persist.dbg.wfc_avail_ovr=1 \
-    persist.radio.VT_HYBRID_ENABLE=1 \
-    persist.vendor.data.iwlan.enable=true
+    persist.vendor.overlay.izat.optin=rro
 
-# IOP properties
+# IOP
 PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.iop.enable_prefetch_ofr=0 \
     vendor.iop.enable_uxe=0
-    vendor.iop.enable_prefetch_ofr=0
 
-# Netflix custom property
+# Media
+PRODUCT_PROPERTY_OVERRIDES += \
+    media.stagefright.thumbnail.prefer_hw_codecs=true \
+    media.settings.xml=/system/etc/media_profiles_vendor.xml \
+    vendor.mm.enable.qcom_parser=63963135
+
+# Memory optimizations
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.qti.sys.fw.bservice_enable=true
+
+# Netflix
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.netflix.bsp_rev=Q855-16947-1
-
-# Paranoid Doze
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sensor.pickup=xiaomi.sensor.pickup \
-    ro.sensor.proximity=true
-
-# Perf
-PRODUCT_PROPERTY_OVERRIDES += \
-    vendor.perf.iop_v3.enable=true
-
-# RCS and IMS
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.rcs.supported=0
 
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
     DEVICE_PROVISIONED=1 \
-    persist.vendor.radio.atfwd.start=true \
+    rild.libpath=/vendor/lib64/libril-qc-hal-qmi.so \
+    ril.subscription.types=RUIM \
+    ro.telephony.default_cdma_sub=0 \
+    ro.telephony.default_network=22,22 \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.dbg.vt_avail_ovr=1 \
+    persist.radio.NO_STAPA=1 \
+    persist.radio.VT_CAM_INTERFACE=1 \
+    persist.vendor.data.iwlan.enable=true \
+    persist.vendor.radio.data_con_rprt=1 \
+    persist.vendor.radio.enable_temp_dds=true \
     persist.vendor.radio.force_on_dc=true \
     persist.vendor.radio.redir_party_num=1 \
     persist.vendor.radio.report_codec=1 \
-    ril.subscription.types=NV,RUIM \
-    ro.telephony.default_network=22,22 \
     telephony.lteOnCdmaDevice=1
 
-# SSR
+# RCS
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.vendor.ssr.enable_ramdumps=0 \
+    persist.rcs.supported=0
+
+# Sensors
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.sensors.enable.mag_filter=true
+
+# Subsystem ramdump
+PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.ssr.restart_level=ALL_ENABLE
+
+# Vendor
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.qti.va_aosp.support=1
